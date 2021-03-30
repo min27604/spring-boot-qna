6fdad65cd0cd8feb3615445cb041bec534361419
Sally Oh <min27604@hotmail.com>
2021-03-30 23:51:07 +0900

    prac

--- src/main/java/com/codessquad/qna/controller/AnswerController.java
+++ src/main/java/com/codessquad/qna/controller/AnswerController.java
@@ -1,20 +1,18 @@
 package com.codessquad.qna.controller;
 
+import com.codessquad.qna.domain.Answer;
 import com.codessquad.qna.domain.User;
 import com.codessquad.qna.service.AnswerService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
 import static com.codessquad.qna.HttpSessionUtils.getUserFromSession;
 import static com.codessquad.qna.HttpSessionUtils.isLoginUser;
 
-@Controller
-@RequestMapping("/questions/{questionId}/answers")
+@RestController
+@RequestMapping("/api/questions/{questionId}/answers")
 public class AnswerController {
     private final AnswerService answerService;
 
@@ -23,14 +21,15 @@ public class AnswerController {
     }
 
     @PostMapping
-    public String create(@PathVariable Long questionId, String contents, HttpSession session) {
+    public Answer create(@PathVariable Long questionId, String contents, HttpSession session) {
         if (!isLoginUser(session)) {
-            return "redirect:/users/login";
+            return null;
+            //return "redirect:/users/login";
         }
 
         User writer = getUserFromSession(session);
-        answerService.save(writer, contents, questionId);
-        return "redirect:/questions/{questionId}";
+        return answerService.save(writer, contents, questionId);
+//        return "redirect:/questions/{questionId}";
     }
 
     @DeleteMapping("/{id}")
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,3 +1,29 @@
+$(".answer-write button[type=submit]").click(addAnswer);
+
+function addAnswer(e) {
+    e.preventDefault();
+    console.log("clicked");
+
+    var queryString = $(".answer-write").serialize();
+    var url = $(".answer-write").attr("action");
+
+    $.ajax({
+        type: 'post',
+        url: url,
+        data : queryString,
+        dataType : 'json',
+        error : onError,
+        success : onSuccess
+    });
+}
+
+function onError() {
+}
+
+function onSuccess() {
+    console.log("hello");
+}
+
 String.prototype.format = function() {
   var args = arguments;
   return this.replace(/{(\d+)}/g, function(match, number) {
